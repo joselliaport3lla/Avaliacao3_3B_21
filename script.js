2a4dfe3527a8131a78dfc5a7c53e12644587977e
Josellia Portella <106090912+joselliaport3lla@users.noreply.github.com>
2024-07-04 09:45:54 -0300

    slide 7 ado 4

--- script.js
+++ script.js
@@ -4,22 +4,26 @@ const caixaAlternativas = document.querySelector(‘.caixa-alternativas’);
 const caixaResultado = document.querySelector(‘.caixa-resultado’);
 const textoResultado = document.querySelector(‘.texto-resultado’);
 
-const perguntas = [ //abre lista de perguntas
-    { //abre objeto de pergunta
+const perguntas = [
+    {
         enunciado: “Insira o enunciado da Pergunta 1”,
         alternativas: [“Alternativa 1”, “Alternativa 2”]
-    }, //fecha objeto de pergunta
+    },
 
-    { //abre objeto de pergunta
+    {
         enunciado: "Insira o enunciado da Pergunta 2",
         alternativas: ["Alternativa 1", "Alternativa 2"]
-    } //fecha objeto de pergunta
+    }
 
-    { //abre objeto de pergunta
+    {
         enunciado: "Insira o enunciado da Pergunta 3",
         alternativas: ["Alternativa 1", "Alternativa 2"]
-    } //fecha objeto de pergunta
-] //fecha lista de prguntas
+    }
+]
 
 let atual = 0;
 let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+}
