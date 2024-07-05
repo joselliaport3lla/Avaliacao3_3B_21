2250a3be339a51324e018f8ab6a4a264b2ce6648
Josellia Portella <106090912+joselliaport3lla@users.noreply.github.com>
2024-07-05 08:02:52 -0300

    slide 05

--- script.js
+++ script.js
@@ -25,5 +25,18 @@ let perguntaAtual;
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado
+    mostraAlternativas();
 }
-mostraPergunta();
+function mostraAlternativas(){
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement(“button”);
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+        enunciado: “Assim que saiu da escola, você se depara com uma nova tecnologia: um chat que consegue responder a todas as dúvidas que uma pessoa pode ter. Além disso, o chat também gera imagens e áudios hiper-realistas. Qual o seu primeiro pensamento?”,
+
+        alternativas: [
+        “Isso é assustador!”,
+        “Isso é maravilhoso!”
+        ]
+        },
+}
